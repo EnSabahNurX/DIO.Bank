c1cf1bdfe45fbc2e7316b261ba0629aad260f9a8
ricardo <ricklimadealmeida@gmail.com>
2021-04-23 15:27:53 -0300

    implementing Balance query

--- Classes/account.cs
+++ Classes/account.cs
@@ -1,36 +1,100 @@
+// using System;
+// namespace DIO.Bank
+// {
+//     // Attributes
+//     public class Account
+//     {
+//         private AccountType AccountType { get; set; }
+//         private double Balance { get; set; }
+//         private double Credit { get; set; }
+//         private string Name { get; set; }
+//     }
+//     // Methods
+//     public Account(AccountType accountType, double balance, double credit, string name)
+//     {
+//         this.AccountType = accountType;
+//         this.Balance = balance;
+//         this.Credit = credit;
+//         this.Name = name;
+//     }
+//     public bool Withdraw(double withdrawAmount)
+//     {
+//         if (this.Balance - withdrawAmount < (this.Credit * -1))
+//         {
+//             Console.WriteLine("Insuficient Balance!");
+//             return false;
+//         }
+//         this.Balance -= withdrawAmount;
+//         Console.WriteLine("The current balance of {0} is {1}", this.Name, this.Balance);
+//         return true;
+//     }
+//     public void Deposit(double depositAmount)
+//     {
+//         this.Balance += depositAmount;
+//         Console.WriteLine("The current balance of {0} is {1}", this.Name, this.Balance);
+//     }
+//     public void Transfer(double transferAmount, Account targetAccount)
+//     {
+//         if (this.Withdraw(transferAmount))
+//         {
+//             targetAccount.Deposit(transferAmount);
+//         }
+//     }
+
+// }
+
 using System;
+
 namespace DIO.Bank
 {
-    // Attributes
     public class Account
     {
+        // Attributes
         private AccountType AccountType { get; set; }
         private double Balance { get; set; }
         private double Credit { get; set; }
         private string Name { get; set; }
-    }
-    // Methods
-    public Account(AccountType accountType, double balance, double credit, string name)
-    {
-        this.AccountType = accountType;
-        this.Balance = balance;
-        this.Credit = credit;
-        this.Name = name;
-    }
-    public bool Withdraw(double withdrawAmount)
-    {
-        if (this.Balance - withdrawAmount < (this.Credit * -1))
+        // Methods
+        public Account(AccountType accountType, double balance, double credit, string nome)
         {
-            Console.WriteLine("Insuficient Balance");
-            return false;
+            this.AccountType = accountType;
+            this.Balance = balance;
+            this.Credit = credit;
+            this.Name = nome;
         }
-        this.Balance -= withdrawAmount;
-        Console.WriteLine("The current balance of {0} is {1}", this.Name, this.Balance);
-        return true;
-    }
-    public void Deposit(double depositValor)
-    {
-        this.Balance += depositValor;
-        Console.WriteLine("The current balance of {0} is {1}", this.Name, this.Balance);
+        public bool Sacar(double valorSaque)
+        {
+            // Validating balance
+            if (this.Balance - valorSaque < (this.Credit * -1))
+            {
+                Console.WriteLine("Insuficient Balance!");
+                return false;
+            }
+            this.Balance -= valorSaque;
+            Console.WriteLine("The current balance of {0} is {1}", this.Name, this.Balance);
+            return true;
+        }
+
+        public void Depositar(double valorDeposito)
+        {
+            this.Balance += valorDeposito;
+            Console.WriteLine("The current balance of {0} is {1}", this.Name, this.Balance);
+        }
+        public void Transferir(double valorTransferencia, Account contaDestino)
+        {
+            if (this.Sacar(valorTransferencia))
+            {
+                contaDestino.Depositar(valorTransferencia);
+            }
+        }
+        public override string ToString()
+        {
+            string display = "";
+            display += "AccountType " + this.AccountType + " | ";
+            display += "Name " + this.Name + " | ";
+            display += "Balance " + this.Balance + " | ";
+            display += "Credit " + this.Credit;
+            return display;
+        }
     }
 }
